da47bc426872024795359234d2c784be42d5f27e
Adam Shimali <adam.shimali@gmail.com>
2019-02-13 16:18:44 +0000

    Group by plan id and order by date.

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
+from sqlalchemy import asc
 
-from application.models import PlanningAuthority
+from application.models import PlanningAuthority, LocalPlan
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -11,8 +12,22 @@ def index():
     return render_template('index.html', planning_authorities=planning_authorities)
 
 
-
 @frontend.route('/local-plans/<planning_authority>')
 def local_plan(planning_authority):
-    planning_authority = PlanningAuthority.query.get(planning_authority)
-    return render_template('local-plans.html', planning_authority=planning_authority)
+    grouped_plans = _group_plans(LocalPlan.query.filter_by(planning_authority_id=planning_authority).order_by(asc(LocalPlan.date)).all())
+    return render_template('local-plans.html', grouped_plans=grouped_plans)
+
+def _group_plans(plans):
+    grouped = []
+    temp = []
+    for plan in plans:
+        if len(temp) == 0:
+            temp.append(plan)
+        elif temp[-1].local_plan == plan.local_plan:
+            temp.append(plan)
+        else:
+            grouped.append(temp)
+            temp = []
+    grouped.append(temp)
+    return grouped
+
