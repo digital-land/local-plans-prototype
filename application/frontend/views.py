ef136c05257ec571483894780e1c037713229603
Adam Shimali <adam.shimali@gmail.com>
2019-02-14 13:42:39 +0000

    Return document id on create so it can be deleted.

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -30,18 +30,21 @@ def add_document_to_plan(planning_authority, local_plan):
         url = request.json['url']
         document = PlanDocument(url=url)
         plan.plan_documents.append(document)
-        resp = {'OK': 200, 'url': url}
+        db.session.add(plan)
+        db.session.commit()
+        resp = {'OK': 200, 'url': url, 'document_id': str(document.id)}
     elif request.json.get('notes') is not None:
         notes = request.json.get('notes')
         plan.notes = notes
+        db.session.add(plan)
+        db.session.commit()
         resp = {'OK': 200, 'notes': notes}
     elif request.json.get('housing_units') is not None:
         housing_units = request.json.get('housing_units')
         plan.number_of_houses = housing_units
+        db.session.add(plan)
+        db.session.commit()
         resp = {'OK': 200, 'housing_units': housing_units}
-
-    db.session.add(plan)
-    db.session.commit()
 
     return jsonify(resp)
 
