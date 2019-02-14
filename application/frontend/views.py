552a3b0539c3463c5848914e183e7bd5983be8e7
Colm Britton <colmjude@gmail.com>
2019-02-14 09:16:04 +0000

    Added autocomplete filter to list of planning authorities

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import asc
 
 from application.models import PlanningAuthority, LocalPlan
@@ -6,16 +6,18 @@ from application.models import PlanningAuthority, LocalPlan
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
 
-@frontend.route('/')
+@frontend.route('/', methods=['GET', 'POST'])
 def index():
     planning_authorities = PlanningAuthority.query.all()
+    if request.method == 'POST':
+        return redirect(url_for('frontend.local_plan', planning_authority=request.form['local-authority-select']))
     return render_template('index.html', planning_authorities=planning_authorities)
 
 @frontend.route('/local-plans/<planning_authority>')
 def local_plan(planning_authority):
-	pla = PlanningAuthority.query.get(planning_authority)
-	grouped_plans = _group_plans(LocalPlan.query.filter_by(planning_authority_id=planning_authority).order_by(asc(LocalPlan.date)).all())
-	return render_template('local-plans.html', grouped_plans=grouped_plans, planning_authority=pla)
+    pla = PlanningAuthority.query.get(planning_authority)
+    grouped_plans = _group_plans(LocalPlan.query.filter_by(planning_authority_id=planning_authority).order_by(asc(LocalPlan.date)).all())
+    return render_template('local-plans.html', grouped_plans=grouped_plans, planning_authority=pla)
 
 def _group_plans(plans):
     grouped = []
