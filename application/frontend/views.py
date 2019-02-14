02406642051cd2304edf07fe6278c300a8406b9c
Adam Shimali <adam.shimali@gmail.com>
2019-02-14 13:28:17 +0000

    Add way to add notes and number of housing units

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -25,10 +25,23 @@ def local_plan(planning_authority):
 @frontend.route('/local-plans/<planning_authority>/<local_plan>', methods=['POST'])
 def add_document_to_plan(planning_authority, local_plan):
     plan = LocalPlan.query.filter_by(planning_authority_id=planning_authority, local_plan=local_plan).one()
-    url = request.json['url']
-    document = PlanDocument(url=url)
-    plan.plan_documents.append(document)
+
+    if request.json.get('url') is not None:
+        url = request.json['url']
+        document = PlanDocument(url=url)
+        plan.plan_documents.append(document)
+        resp = {'OK': 200, 'url': url}
+    elif request.json.get('notes') is not None:
+        notes = request.json.get('notes')
+        plan.notes = notes
+        resp = {'OK': 200, 'notes': notes}
+    elif request.json.get('housing_units') is not None:
+        housing_units = request.json.get('housing_units')
+        plan.number_of_houses = housing_units
+        resp = {'OK': 200, 'housing_units': housing_units}
+
     db.session.add(plan)
     db.session.commit()
 
-    return jsonify({'OK': 200, 'url': url})
+    return jsonify(resp)
+
