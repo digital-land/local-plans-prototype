a2e135e1cfd08d773ac4b1b82eafa877796525e0
Colm Britton <colmjude@gmail.com>
2019-02-26 12:20:26 +0800

    Put latest plan as first tab

--- application/filters.py
+++ application/filters.py
@@ -1,3 +1,4 @@
+from jinja2 import Undefined
 
 def format_date(date_time):
     return date_time.strftime('%d %b %Y')
@@ -5,3 +6,15 @@ def format_date(date_time):
 
 def format_month_and_year(date_time):
     return date_time.strftime('%b %Y')
+
+
+def last_state_date(plan):
+	states = plan.ordered_states()
+	return states[-1]
+
+
+def sort_plans(plans):
+	if plans is None or isinstance(plans, Undefined) or len(plans) == 1:
+		return plans
+
+	return sorted(plans, key=last_state_date, reverse=True)
