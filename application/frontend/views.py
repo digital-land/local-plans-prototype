7f88342c88fd20d648943659a613add3f0b71456
Adam Shimali <adam.shimali@gmail.com>
2019-03-21 15:35:08 +0000

    Check if document already added to this plan or planning authority

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -216,7 +216,7 @@ def add_document_to_plan(planning_authority):
         if document_type == 'plan_document':
             local_plan = request.args.get('local_plan')
             add_to = LocalPlan.query.get(local_plan)
-            document = PlanDocument.query.filter_by(url=url).first()
+            document = PlanDocument.query.filter_by(url=url, local_plan=add_to).first()
             if document is None:
                 document = PlanDocument(url=url, title=request.json['doc_title'])
                 add_to.plan_documents.append(document)
@@ -232,13 +232,13 @@ def add_document_to_plan(planning_authority):
 
         elif document_type == 'emerging_plan_document':
             # TODO - update this to other_document
-            document = OtherDocument.query.filter_by(url=url).first()
+            add_to = PlanningAuthority.query.get(planning_authority)
+            document = OtherDocument.query.filter_by(url=url, planning_authority=add_to).first()
             if document is None:
                 if request.json.get('doc_title') is not None:
                     document = OtherDocument(url=url, title=request.json.get('doc_title'))
                 else:
                     document = OtherDocument(url=url, title='Local Development Scheme')
-                add_to = PlanningAuthority.query.get(planning_authority)
                 add_to.other_documents.append(document)
                 db.session.add(add_to)
                 db.session.commit()
