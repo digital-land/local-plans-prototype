38b0f20d758f01daf3c7d1726f8988a1fd4e1d78
Adam Shimali <adam.shimali@gmail.com>
2019-03-27 16:08:01 +0000

    Reduced caching time for flask optimize

--- application/extensions.py
+++ application/extensions.py
@@ -10,5 +10,12 @@ db = SQLAlchemy()
 from flask_migrate import Migrate
 migrate = Migrate(db=db)
 
+config = {
+    'html': {'htmlmin': True,  'compress': True, 'cache': 'GET-60'},
+    'json': {'htmlmin': False, 'compress': True, 'cache': False},
+    'text': {'htmlmin': False, 'compress': True, 'cache': 'GET-60'},
+    'trim_fragment': False,
+}
+
 from flask_optimize import FlaskOptimize
-flask_optimize = FlaskOptimize()
+flask_optimize = FlaskOptimize(config=config)
