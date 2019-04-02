73edd796c94eb0c04f0b588dc28b7ef96c3dc08c
Colm Britton <colmjude@gmail.com>
2019-04-02 13:53:38 +0800

    Fix up mhclg-maps.js

--- src/js/mhclg-maps.js
+++ src/js/mhclg-maps.js
@@ -1,9 +1,11 @@
+/*global L */
+
 (function(L){
-  const MHCLGMaps = L.Class.extend({  
+  const MHCLGMaps = L.Class.extend({
     // this is the constructor
     initialize: function (options) {
-      if(!options.mapbox_token) {
-        console.warn('Must provide Mapbox token');
+      if (!options.mapbox_token) {
+        console.warn("Must provide Mapbox token"); // eslint-disable-line
         return false;
       }
       L.Util.setOptions(this, options);
@@ -13,13 +15,13 @@
     // these are the default options
     options: {
       maxZoom: 18,
-      mapHeight: '800px'
+      mapHeight: "800px"
     },
 
     // this is a public function
     createMap: function(idSelector, options) {
-      var options = options || {};
-      const map = L.map(idSelector, options);
+      var opts = options || {};
+      const map = L.map(idSelector, opts);
       this.addTileLayer(map);
       this.setMapContainerHeight(map);
       return map;
@@ -27,21 +29,21 @@
 
     addTileLayer: function(map) {
       L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${this.options.mapbox_token}`, {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: this.options.maxZoom,
-          id: 'mapbox.streets',
-          accessToken: '{{ config.MAPBOX_TOKEN }}'
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: this.options.maxZoom,
+        id: "mapbox.streets",
+        accessToken: "{{ config.MAPBOX_TOKEN }}"
       }).addTo(map);
     },
 
     setMapContainerHeight: function(map, value, isRatio) {
-      var ratio = value || (2/3);
+      var ratio = value || 2 / 3;
       // set a default value if none provided are height
       // not set with CSS
-      if( !value && !map._container.style.height) {
+      if (!value && !map._container.style.height) {
         map._container.style.height = this.options.mapHeight;
       }
-      if( isRatio ) {
+      if (isRatio) {
         var width = map._container.offsetWidth;
         map._container.style.height = `${width * ratio}px`;
       } else {
